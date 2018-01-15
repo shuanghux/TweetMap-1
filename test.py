f90513665a9e6169d9c50eb3e1a52f7cc6eda673
Shuhui(Alisa) He <hsh0712@hotmail.com>
2018-01-14 20:36:40 -0800

    index data in ES & search qurey

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from elasticsearch import Elasticsearch
 from flask import Flask, request, jsonify, render_template, send_file
 from flask_cors import CORS, cross_origin
 
-# es=Elasticsearch(['https://search-tweetmap-px7q55rz5bmnnz3iuzedyvr4ta.us-west-1.es.amazonaws.com/',])
+
 
 # print es.search(index="tweets", body={"query": {"match": {'text':{'query': "Bank"}}}})
 
@@ -17,12 +17,13 @@ from flask_cors import CORS, cross_origin
 # 		es.index(index='tweets', doc_type='listings', body=json.loads(json.dumps(data)))
 
 
-def search(target):
+#def search(target):
 
-	es.search(index="tweets", body={"query": {"match": {'text':{'query': target}}}})
+	#es.search(index="tweets", body={"query": {"match": {'text':{'query': target}}}})
 
 
 
+es=Elasticsearch(['https://search-tweetmap-px7q55rz5bmnnz3iuzedyvr4ta.us-west-1.es.amazonaws.com/',])
 application = Flask(__name__)
 CORS(application)
 
@@ -35,8 +36,39 @@ def index():
 @application.route('/store', methods=['POST'])
 def store():
 	jsonData = request.get_json()
-	print json.dumps(jsonData)
-	return "received"
+	#print json.dumps(jsonData)
+
+	jsonArr = jsonData["matched_results"]
+
+
+	#index every single post in ES
+	for singlePost in jsonArr:
+
+		es.index(index='posts', doc_type='tweets', body=singlePost)
+
+
+	#search()
+
+	return jsonify(**{"result":"OK"})
+
+
+
+
+@application.route('/searchKey', methods=['GET'])	
+def search():
+
+	searchword = request.args.get('key', '')
+
+	jsonObj = es.search(index='posts', size=10000, body={"query": {"match": {'text':{'query': searchword}}}})
+
+	print json.dumps(jsonObj)
+
+	return jsonify(**jsonObj)
+
+
+
+
+	
 
 
 if __name__ == "__main__":
