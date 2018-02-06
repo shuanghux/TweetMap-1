0995c2f491e373a71924b2d8a9c92c6d04cf30e0
Jitong-Liu <jitong.liu@outlook.com>
2018-02-06 00:59:01 -0800

    tweets update

--- static/js/main.js
+++ static/js/main.js
@@ -4,11 +4,13 @@
 var responseText;
 var sampleStr;
 var jsonArr;
+var updateJsonArr;
 // map object
 var map;
 var heatmap;
 // detail html for each tweet
 var webViewArr;
+var markerSize = 0;
 var markers;
 var markerCluster;
 var firstTime = true;
@@ -274,6 +276,7 @@ function initVariables() {
 	jsonArr = [];
 	webViewArr = [];
 	markers = [];
+	updateJsonArr = [];
 }
 
 function getQuote() {
@@ -285,7 +288,7 @@ function getQuote() {
 	}
 	var keyword = $("#search_box").val();
 	$.ajax({
-		url: 'http://tweetymap.us-east-2.elasticbeanstalk.com//searchKey',
+		url: 'http://tweetymap.us-east-2.elasticbeanstalk.com/searchKey',
 		// url: 'http://tweetymap.us-east-2.elasticbeanstalk.com/searchKey',
 		type: 'GET',
 	//dataType: 'json',
@@ -295,9 +298,7 @@ function getQuote() {
 		console.log("success");
 		responseText = response;
 		processData(JSON.parse(response)["results"]);
-
-	// heatMap();
-	addMarkers();
+		addMarkers();
 	})
 	.fail(function() {
 		console.log("error");
@@ -305,7 +306,43 @@ function getQuote() {
 	.always(function() {
 		console.log("complete");
 	});
+
+	setInterval(updateTweets, 30000);
 	firstTime = false;
+}
+
+function updateTweets(){
+
+	$.ajax({
+		url: 'http://tweetymap.us-east-2.elasticbeanstalk.com/update',
+		type: 'GET', 
+		dataType: 'json',
+	}).done(function(response) {
+		console.log("update success");
+		processUpdateData(response["result"]);
+		updateMarkers();
+	}).fail(function() {
+		console.log("update error");
+	});
+}
+
+function updateMarkers(){
+	if (updateJsonArr.length!=0) {
+		removeMarkers();
+
+		for (var i in updateJsonArr) {
+
+			jsonArr.push(updateJsonArr[i]);
+
+		}
+		addMarkers();
+	}
+	
+}
+
+function processUpdateData(response){
+	updateJsonArr = response;
+	console.log(updateJsonArr);
 }
 
 function processData(response) {
@@ -328,8 +365,9 @@ function addMarkers() {
 	$("#graph").hide();
 	$("#map").show();
 	$("#text").html("");
-	webViewArr = new Array(jsonArr.length);
-	markers = new Array(jsonArr.length);
+	webViewArr = new Array();
+	markers = new Array();
+	markerSize = jsonArr.length;
 
 	for (var i in jsonArr) {
 		var currObj = jsonArr[i];
@@ -355,38 +393,6 @@ function addMarkers() {
 function heatMap() {
 	isHeatmap = true;
 	var heatmapData = [];
-	// var gradient = [
-	// 	'rgba(0, 255, 255, 0)',
-	// 	'rgba(0, 255, 255, 1)',
-	// 	'rgba(0, 191, 255, 1)',
-	// 	'rgba(0, 127, 255, 1)',
-	// 	'rgba(0, 63, 255, 1)',
-	// 	'rgba(0, 0, 255, 1)',
-	// 	'rgba(0, 0, 223, 1)',
-	// 	'rgba(0, 0, 191, 1)',
-	// 	'rgba(0, 0, 159, 1)',
-	// 	'rgba(0, 0, 127, 1)',
-	// 	'rgba(63, 0, 91, 1)',
-	// 	'rgba(127, 0, 63, 1)',
-	// 	'rgba(191, 0, 31, 1)',
-	// 	'rgba(255, 0, 0, 1)'
-	// ]
-	// var gradient = [
- //          'rgba(0, 255, 255, 0)',
- //          'rgba(0, 255, 255, 1)',
- //          'rgba(0, 191, 255, 1)',
- //          'rgba(0, 127, 255, 1)',
- //          'rgba(0, 63, 255, 1)',
- //          'rgba(0, 0, 255, 1)',
- //          'rgba(0, 0, 223, 1)',
- //          'rgba(0, 0, 191, 1)',
- //          'rgba(0, 0, 159, 1)',
- //          'rgba(0, 0, 127, 1)',
- //          'rgba(63, 0, 91, 1)',
- //          'rgba(127, 0, 63, 1)',
- //          'rgba(191, 0, 31, 1)',
- //          'rgba(255, 0, 0, 1)'
- //        ]
     var gradient = [
       'rgba(0, 255, 255, 0)',
       'rgba(0, 255, 210, 1)',
@@ -479,34 +485,6 @@ function showSentiment(){
 	$("#text").html("Overall sentiment is " + parseFloat(Math.round(sentimentValue * 100) / 100).toFixed(2) +"%");
 	$("#map").hide();
 	$("#graph").show();
-
-	// var json = {"countries_msg_vol": {
-	// 	"CA": {"tweets": 0, "polarity": 0},"GB": {"tweets": 0, "polarity": 0},"AU": {"tweets": 0, "polarity": 0},"DE": {"tweets": 0, "polarity": 0},"AX": {"tweets": 0, "polarity": 0},"AF": {"tweets": 0, "polarity": 0},"AL": {"tweets": 0, "polarity": 0},"DZ": {"tweets": 0, "polarity": 0},"AS": {"tweets": 0, "polarity": 0},"AD": {"tweets": 0, "polarity": 0},
-	// 	"AO": {"tweets": 0, "polarity": 0},"AI": {"tweets": 0, "polarity": 0},"AQ": {"tweets": 0, "polarity": 0},"AG": {"tweets": 0, "polarity": 0},"AR": {"tweets": 0, "polarity": 0},"AM": {"tweets": 0, "polarity": 0},"AW": {"tweets": 0, "polarity": 0},"AU": {"tweets": 0, "polarity": 0},"AT": {"tweets": 0, "polarity": 0},"AZ": {"tweets": 0, "polarity": 0},
-	// 	"BS": {"tweets": 0, "polarity": 0},"BH": {"tweets": 0, "polarity": 0},"BD": {"tweets": 0, "polarity": 0},"BB": {"tweets": 0, "polarity": 0},"BY": {"tweets": 0, "polarity": 0},"BE": {"tweets": 0, "polarity": 0},"BZ": {"tweets": 0, "polarity": 0},"BJ": {"tweets": 0, "polarity": 0},"BM": {"tweets": 0, "polarity": 0},"BT": {"tweets": 0, "polarity": 0},
-	// 	"BO": {"tweets": 0, "polarity": 0},"BA": {"tweets": 0, "polarity": 0},"BW": {"tweets": 0, "polarity": 0},"BV": {"tweets": 0, "polarity": 0},"BR": {"tweets": 0, "polarity": 0},"IO": {"tweets": 0, "polarity": 0},"BN": {"tweets": 0, "polarity": 0},"BG": {"tweets": 0, "polarity": 0},"BF": {"tweets": 0, "polarity": 0},"BI": {"tweets": 0, "polarity": 0},
-	// 	"KH": {"tweets": 0, "polarity": 0},"CM": {"tweets": 0, "polarity": 0},"CA": {"tweets": 0, "polarity": 0},"CV": {"tweets": 0, "polarity": 0},"KY": {"tweets": 0, "polarity": 0},"CF": {"tweets": 0, "polarity": 0},"TD": {"tweets": 0, "polarity": 0},"CL": {"tweets": 0, "polarity": 0},"CN": {"tweets": 0, "polarity": 0},"CX": {"tweets": 0, "polarity": 0},
-	// 	"CC": {"tweets": 0, "polarity": 0},"CO": {"tweets": 0, "polarity": 0},"KM": {"tweets": 0, "polarity": 0},"CG": {"tweets": 0, "polarity": 0},"CD": {"tweets": 0, "polarity": 0},"CK": {"tweets": 0, "polarity": 0},"CR": {"tweets": 0, "polarity": 0},"CI": {"tweets": 0, "polarity": 0},"HR": {"tweets": 0, "polarity": 0},"CU": {"tweets": 0, "polarity": 0},
-	// 	"CY": {"tweets": 0, "polarity": 0},"CZ": {"tweets": 0, "polarity": 0},"DK": {"tweets": 0, "polarity": 0},"DJ": {"tweets": 0, "polarity": 0},"DM": {"tweets": 0, "polarity": 0},"DO": {"tweets": 0, "polarity": 0},"TP": {"tweets": 0, "polarity": 0},"EC": {"tweets": 0, "polarity": 0},"EG": {"tweets": 0, "polarity": 0},"SV": {"tweets": 0, "polarity": 0},
-	// 	"GQ": {"tweets": 0, "polarity": 0},"ER": {"tweets": 0, "polarity": 0},"EE": {"tweets": 0, "polarity": 0},"ET": {"tweets": 0, "polarity": 0},"FK": {"tweets": 0, "polarity": 0},"FO": {"tweets": 0, "polarity": 0},"FJ": {"tweets": 0, "polarity": 0},"FI": {"tweets": 0, "polarity": 0},"FR": {"tweets": 0, "polarity": 0},"FX": {"tweets": 0, "polarity": 0},
-	// 	"GF": {"tweets": 0, "polarity": 0},"PF": {"tweets": 0, "polarity": 0},"TF": {"tweets": 0, "polarity": 0},"GA": {"tweets": 0, "polarity": 0},"GM": {"tweets": 0, "polarity": 0},"GE": {"tweets": 0, "polarity": 0},"DE": {"tweets": 0, "polarity": 0},"GH": {"tweets": 0, "polarity": 0},"GI": {"tweets": 0, "polarity": 0},"GR": {"tweets": 0, "polarity": 0},
-	// 	"GL": {"tweets": 0, "polarity": 0},"GD": {"tweets": 0, "polarity": 0},"GP": {"tweets": 0, "polarity": 0},"GU": {"tweets": 0, "polarity": 0},"GT": {"tweets": 0, "polarity": 0},"GG": {"tweets": 0, "polarity": 0},"GN": {"tweets": 0, "polarity": 0},"GW": {"tweets": 0, "polarity": 0},"GY": {"tweets": 0, "polarity": 0},"HT": {"tweets": 0, "polarity": 0},
-	// 	"HM": {"tweets": 0, "polarity": 0},"HN": {"tweets": 0, "polarity": 0},"HU": {"tweets": 0, "polarity": 0},"IS": {"tweets": 0, "polarity": 0},"IN": {"tweets": 0, "polarity": 0},"ID": {"tweets": 0, "polarity": 0},"IR": {"tweets": 0, "polarity": 0},"IQ": {"tweets": 0, "polarity": 0},"IE": {"tweets": 0, "polarity": 0},"IM": {"tweets": 0, "polarity": 0},
-	// 	"IL": {"tweets": 0, "polarity": 0},"IT": {"tweets": 0, "polarity": 0},"JM": {"tweets": 0, "polarity": 0},"JP": {"tweets": 0, "polarity": 0},"JE": {"tweets": 0, "polarity": 0},"JO": {"tweets": 0, "polarity": 0},"KZ": {"tweets": 0, "polarity": 0},"KE": {"tweets": 0, "polarity": 0},"KI": {"tweets": 0, "polarity": 0},"KR": {"tweets": 0, "polarity": 0},
-	// 	"KP": {"tweets": 0, "polarity": 0},"KW": {"tweets": 0, "polarity": 0},"KG": {"tweets": 0, "polarity": 0},"LA": {"tweets": 0, "polarity": 0},"LV": {"tweets": 0, "polarity": 0},"LB": {"tweets": 0, "polarity": 0},"LS": {"tweets": 0, "polarity": 0},"LR": {"tweets": 0, "polarity": 0},"LY": {"tweets": 0, "polarity": 0},"LI": {"tweets": 0, "polarity": 0},
-	// 	"LT": {"tweets": 0, "polarity": 0},"LU": {"tweets": 0, "polarity": 0},"MO": {"tweets": 0, "polarity": 0},"MK": {"tweets": 0, "polarity": 0},"MG": {"tweets": 0, "polarity": 0},"MW": {"tweets": 0, "polarity": 0},"MY": {"tweets": 0, "polarity": 0},"MV": {"tweets": 0, "polarity": 0},"ML": {"tweets": 0, "polarity": 0},"MT": {"tweets": 0, "polarity": 0},
-	// 	"MH": {"tweets": 0, "polarity": 0},"MQ": {"tweets": 0, "polarity": 0},"MR": {"tweets": 0, "polarity": 0},"MU": {"tweets": 0, "polarity": 0},"YT": {"tweets": 0, "polarity": 0},"MX": {"tweets": 0, "polarity": 0},"FM": {"tweets": 0, "polarity": 0},"MD": {"tweets": 0, "polarity": 0},"MC": {"tweets": 0, "polarity": 0},"MN": {"tweets": 0, "polarity": 0},
-	// 	"ME": {"tweets": 0, "polarity": 0},"MS": {"tweets": 0, "polarity": 0},"MA": {"tweets": 0, "polarity": 0},"MZ": {"tweets": 0, "polarity": 0},"MM": {"tweets": 0, "polarity": 0},"NA": {"tweets": 0, "polarity": 0},"NR": {"tweets": 0, "polarity": 0},"NP": {"tweets": 0, "polarity": 0},"NL": {"tweets": 0, "polarity": 0},"AN": {"tweets": 0, "polarity": 0},
-	// 	"NC": {"tweets": 0, "polarity": 0},"NZ": {"tweets": 0, "polarity": 0},"NI": {"tweets": 0, "polarity": 0},"NE": {"tweets": 0, "polarity": 0},"NG": {"tweets": 0, "polarity": 0},"NU": {"tweets": 0, "polarity": 0},"NF": {"tweets": 0, "polarity": 0},"MP": {"tweets": 0, "polarity": 0},"NO": {"tweets": 0, "polarity": 0},"OM": {"tweets": 0, "polarity": 0},
-	// 	"PK": {"tweets": 0, "polarity": 0},"PW": {"tweets": 0, "polarity": 0},"PS": {"tweets": 0, "polarity": 0},"PA": {"tweets": 0, "polarity": 0},"PG": {"tweets": 0, "polarity": 0},"PY": {"tweets": 0, "polarity": 0},"PE": {"tweets": 0, "polarity": 0},"PH": {"tweets": 0, "polarity": 0},"PN": {"tweets": 0, "polarity": 0},"PL": {"tweets": 0, "polarity": 0},
-	// 	"PT": {"tweets": 0, "polarity": 0},"PR": {"tweets": 0, "polarity": 0},"QA": {"tweets": 0, "polarity": 0},"RO": {"tweets": 0, "polarity": 0},"RU": {"tweets": 0, "polarity": 0},"RW": {"tweets": 0, "polarity": 0},"SH": {"tweets": 0, "polarity": 0},"KN": {"tweets": 0, "polarity": 0},"LC": {"tweets": 0, "polarity": 0},"PM": {"tweets": 0, "polarity": 0},
-	// 	"VC": {"tweets": 0, "polarity": 0},"WS": {"tweets": 0, "polarity": 0},"SM": {"tweets": 0, "polarity": 0},"ST": {"tweets": 0, "polarity": 0},"SA": {"tweets": 0, "polarity": 0},"SN": {"tweets": 0, "polarity": 0},"RS": {"tweets": 0, "polarity": 0},"SC": {"tweets": 0, "polarity": 0},"SL": {"tweets": 0, "polarity": 0},"SG": {"tweets": 0, "polarity": 0},
-	// 	"SK": {"tweets": 0, "polarity": 0},"SI": {"tweets": 0, "polarity": 0},"SB": {"tweets": 0, "polarity": 0},"SO": {"tweets": 0, "polarity": 0},"ZA": {"tweets": 0, "polarity": 0},"ES": {"tweets": 0, "polarity": 0},"LK": {"tweets": 0, "polarity": 0},"SD": {"tweets": 0, "polarity": 0},"SR": {"tweets": 0, "polarity": 0},"SJ": {"tweets": 0, "polarity": 0},
-	// 	"SZ": {"tweets": 0, "polarity": 0},"SE": {"tweets": 0, "polarity": 0},"CH": {"tweets": 0, "polarity": 0},"SY": {"tweets": 0, "polarity": 0},"TW": {"tweets": 0, "polarity": 0},"TJ": {"tweets": 0, "polarity": 0},"TZ": {"tweets": 0, "polarity": 0},"TH": {"tweets": 0, "polarity": 0},"TL": {"tweets": 0, "polarity": 0},"TG": {"tweets": 0, "polarity": 0},
-	// 	"TK": {"tweets": 0, "polarity": 0},"TO": {"tweets": 0, "polarity": 0},"TT": {"tweets": 0, "polarity": 0},"TN": {"tweets": 0, "polarity": 0},"TR": {"tweets": 0, "polarity": 0},"TM": {"tweets": 0, "polarity": 0},"TC": {"tweets": 0, "polarity": 0},"TV": {"tweets": 0, "polarity": 0},"UG": {"tweets": 0, "polarity": 0},"UA": {"tweets": 0, "polarity": 0},
-	// 	"AE": {"tweets": 0, "polarity": 0},"GB": {"tweets": 0, "polarity": 0},"US": {"tweets": 0, "polarity": 0},"UM": {"tweets": 0, "polarity": 0},"UY": {"tweets": 0, "polarity": 0},"UZ": {"tweets": 0, "polarity": 0},"VU": {"tweets": 0, "polarity": 0},"VA": {"tweets": 0, "polarity": 0},"VE": {"tweets": 0, "polarity": 0},"VN": {"tweets": 0, "polarity": 0},
-	// 	"VG": {"tweets": 0, "polarity": 0},"VI": {"tweets": 0, "polarity": 0},"WF": {"tweets": 0, "polarity": 0},"YE": {"tweets": 0, "polarity": 0},"ZM": {"tweets": 0, "polarity": 0},"ZW": {"tweets": 0, "polarity": 0}
-	// }};
 
 	var sentiments = new Object();
 
